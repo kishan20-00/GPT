264b043c91f00d069516e69b0f2e6583702434bd
Kishan Rajendra <86339947+kishan20-00@users.noreply.github.com>
2025-04-09 12:14:39 +0530

    Updated Backend configurations

--- backend/api/main.py
+++ backend/api/main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                   "https://gpt-2ait.vercel.app",
+                   "https://gpt-ten-pi-48.vercel.app",
                    "https://gpt-seven-sand.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
